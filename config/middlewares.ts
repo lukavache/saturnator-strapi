e2b47e6bdffaf5b2eb372a24d0686cb4e421c13c
Luka Vacheishvili <lukavacheishvili@Lukas-MacBook-Pro.local>
2025-07-26 19:41:21 +0400

    first commit

---
+++ config/middlewares.ts
@@ -0,0 +1,32 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http://localhost:3000', 'http://localhost:3001'],
+          'img-src': ["'self'", 'data:', 'blob:', 'http://localhost:1337'],
+          'media-src': ["'self'", 'data:', 'blob:', 'http://localhost:1337'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3003']
+    }
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
---
+++ config/plugins.ts
@@ -0,0 +1,45 @@
+export default ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwt: {
+        expiresIn: '7d',
+      },
+      email: {
+        confirmation: {
+          template: {
+            subject: 'Welcome to Saturnator! Please confirm your email',
+            text: 'Welcome to Saturnator! Please confirm your email by clicking on the following link: <%= URL %>',
+            html: `
+              <h1>Welcome to Saturnator!</h1>
+              <p>Please confirm your email by clicking on the following link:</p>
+              <a href="<%= URL %>">Confirm Email</a>
+            `
+          }
+        },
+        resetPassword: {
+          template: {
+            subject: 'Reset your password',
+            text: 'Reset your password by clicking on the following link: <%= URL %>',
+            html: `
+              <h1>Reset your password</h1>
+              <p>Reset your password by clicking on the following link:</p>
+              <a href="<%= URL %>">Reset Password</a>
+            `
+          }
+        }
+      }
+    },
+  },
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: env('SENDGRID_FROM_EMAIL'),
+        defaultReplyTo: env('SENDGRID_FROM_EMAIL'),
+      },
+    },
+  },
+});
