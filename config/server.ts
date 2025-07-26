9432b23df7f91e72bc9c291afb37f8bc2bf86603
Luka Vacheishvili <lukavacheishvili@Lukas-MacBook-Pro.local>
2025-07-26 23:51:10 +0400

    disabled analytics

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,9 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  telemetry: {
+    enabled: false,
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
